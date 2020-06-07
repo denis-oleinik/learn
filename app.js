1f65772e1a7dc658cfcab9c0f1d0a98b9df6c189
Denis Oleinik <denador@mail.ru>
2020-06-07 21:58:50 +0600

    условия и циклы

--- app.js
+++ app.js
@@ -10,8 +10,24 @@ const personalMovieDB = {
 for(let i = 0; i < 2; i++){
     const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
         rating = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[lastFilm] = rating;
+
+    if(lastFilm != null && rating != null && lastFilm != '' && rating != '' && lastFilm.length < 50){
+        personalMovieDB.movies[lastFilm] = rating;
+    }else{
+        alert('Чтобы продолжить необходимо заполнить данные');
+        i--;
+    }
+    
+}
+let viewer = '';
+if(personalMovieDB.count < 10) {
+    viewer = 'Просмотрено довольно мало фильмов';
+}else if(personalMovieDB.count < 30) {
+    viewer = 'Вы классический зритель';
+}else{
+    viewer = 'Вы киноман';
 }
 
-console.log(personalMovieDB);
+alert(viewer);
+
 
